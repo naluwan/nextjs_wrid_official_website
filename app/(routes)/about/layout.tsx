5f7470a5afdee67b80ef520670ad359d43ee8697
naluwan <street.baller06@gmail.com>
2023-11-27 13:32:43 +0800

    feat: add About page

---
+++ app/(routes)/about/layout.tsx
@@ -0,0 +1,9 @@
+import { Metadata } from 'next';
+import Page from './page';
+
+export const metadata: Metadata = {
+  title: '關於我們',
+  description: '',
+};
+
+export default Page;
---
+++ app/(routes)/about/page.tsx
@@ -0,0 +1,5 @@
+const AboutPage = () => {
+  return <div className='h-screen'>about page</div>;
+};
+
+export default AboutPage;
