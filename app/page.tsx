15cac56ff253f10f9193a610f9886aafbf8cafdb
naluwan <street.baller06@gmail.com>
2023-11-27 13:32:09 +0800

    feat: update Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,114 +1,124 @@
+'use client';
+
+import { useEffect } from 'react';
+import Link from 'next/link';
 import Image from 'next/image';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+import logo from '@/public/logo.png';
 
 const Home = () => {
+  useEffect(() => {
+    // 初始化 AOS 套件
+    AOS.init();
+  }, []); // 第二個參數是空數組，表示只在組件掛載時執行一次
+
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <div className='z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex'>
-        <p className='fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30'>
-          Get started by editing&nbsp;
-          <code className='font-mono font-bold'>app/page.tsx</code>
-        </p>
-        <div className='fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none'>
-          <a
-            className='pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0'
-            href='https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            By{' '}
-            <Image
-              src='/vercel.svg'
-              alt='Vercel Logo'
-              className='dark:invert'
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className='bg-mi-bai h-full'>
+      <header className='flex items-end justify-between p-4'>
+        {/* 公司Logo */}
+        <div className='flex items-center'>
+          <Image src={logo} alt='logo' width={75} height={75} />
         </div>
-      </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className='relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert'
-          src='/next.svg'
-          alt='Next.js Logo'
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        {/* 導航菜單 */}
+        <nav className='mt-4'>
+          <ul className='flex space-x-4'>
+            <li>
+              <Link href='/portfolio'>作品集</Link>
+            </li>
+            <li>
+              <Link href='/about'>關於我們</Link>
+            </li>
+            <li>
+              <Link href='/contact'>聯繫我們</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
 
-      <div className='mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left'>
-        <a
-          href='https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
-          className='group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          <h2 className={'mb-3 text-2xl font-semibold'}>
-            Docs{' '}
-            <span className='inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none'>
-              -&gt;
-            </span>
-          </h2>
-          <p className={'m-0 max-w-[30ch] text-sm opacity-50'}>
-            Find in-depth information about Next.js features and API.
+      <main className='p-8'>
+        {/* 引導語 */}
+        <section className='text-center'>
+          <div className='flex flex-col items-center justify-center md:flex-row'>
+            <h2 className='mb-4 text-3xl font-bold' data-aos='fade-up'>
+              家乃身心歸依沉澱之處
+            </h2>
+            <h2 className='mb-4 hidden text-3xl font-bold md:block' data-aos='fade-up'>
+              ，
+            </h2>
+            <h2 className='mb-4 text-3xl font-bold' data-aos='fade-up'>
+              亦為喧囂中的一方淨土
+            </h2>
+          </div>
+
+          <p className='text-lg' data-aos='fade-up' data-aos-delay='200'>
+            故以居者為核心發想
+            <br />
+            實亦輔虛，虛亦成境
           </p>
-        </a>
-
-        <a
-          href='https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app'
-          className='group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          <h2 className={'mb-3 text-2xl font-semibold'}>
-            Learn{' '}
-            <span className='inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none'>
-              -&gt;
-            </span>
-          </h2>
-          <p className={'m-0 max-w-[30ch] text-sm opacity-50'}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
+          <p className='text-lg' data-aos='fade-up' data-aos-delay='400'>
+            設計核心不侷限於實體架構
+            <br />
+            而是空間、人、自然的連結性
+            <br />
+            回到大地本然的屬性
+            <br />
+            重新構建出生活的本體
           </p>
-        </a>
+        </section>
 
-        <a
-          href='https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
-          className='group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          <h2 className={'mb-3 text-2xl font-semibold'}>
-            Templates{' '}
-            <span className='inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none'>
-              -&gt;
-            </span>
+        {/* 精選案例 */}
+        <section className='mt-8'>
+          <h2 className='mb-4 text-2xl font-bold' data-aos='fade-up' data-aos-delay='600'>
+            精選案例
           </h2>
-          <p className={'m-0 max-w-[30ch] text-sm opacity-50'}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
+          {/* 這裡可以使用輪播圖或其他方式展示案例 */}
+          <div
+            className='grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3'
+            data-aos='fade-up'
+            data-aos-delay='600'
+          >
+            <div className='rounded-lg bg-white p-4 shadow-md'>
+              <Image src='/portfolio/1.jpg' alt='案例一' width={300} height={200} />
+              <p className='mt-2'>現代簡約風格</p>
+            </div>
+            {/* 其他案例卡片 */}
+            <div className='rounded-lg bg-white p-4 shadow-md'>
+              <Image src='/portfolio/1.jpg' alt='案例一' width={300} height={200} />
+              <p className='mt-2'>現代簡約風格</p>
+            </div>
 
-        <a
-          href='https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
-          className='group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          <h2 className={'mb-3 text-2xl font-semibold'}>
-            Deploy{' '}
-            <span className='inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none'>
-              -&gt;
-            </span>
-          </h2>
-          <p className={'m-0 max-w-[30ch] text-sm opacity-50'}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+            <div className='rounded-lg bg-white p-4 shadow-md'>
+              <Image src='/portfolio/1.jpg' alt='案例一' width={300} height={200} />
+              <p className='mt-2'>現代簡約風格</p>
+            </div>
+          </div>
+
+          <div
+            className='mt-8 grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3'
+            data-aos='fade-up'
+            data-aos-delay='800'
+          >
+            <div className='rounded-lg bg-white p-4 shadow-md'>
+              <Image src='/portfolio/1.jpg' alt='案例一' width={300} height={200} />
+              <p className='mt-2'>現代簡約風格</p>
+            </div>
+            {/* 其他案例卡片 */}
+            <div className='rounded-lg bg-white p-4 shadow-md'>
+              <Image src='/portfolio/1.jpg' alt='案例一' width={300} height={200} />
+              <p className='mt-2'>現代簡約風格</p>
+            </div>
+
+            <div className='rounded-lg bg-white p-4 shadow-md'>
+              <Image src='/portfolio/1.jpg' alt='案例一' width={300} height={200} />
+              <p className='mt-2'>現代簡約風格</p>
+            </div>
+          </div>
+        </section>
+      </main>
+    </div>
   );
 };
 
