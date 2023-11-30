83d3fb099c179286b0e8566ab369ebc1999c07ea
naluwan <street.baller06@gmail.com>
2023-11-30 13:37:49 +0800

    fix: render error

--- components/Modal/Modal.tsx
+++ components/Modal/Modal.tsx
@@ -11,41 +11,41 @@ type ModalProps = {
 
 const Modal: React.FC<ModalProps> = (props) => {
   const { open, onClose, children } = props;
-
-  if (typeof window === 'object') {
-    return ReactDOM.createPortal(
-      // backdrop
-      <div
-        onClick={onClose}
-        className={`fixed inset-0 z-50 flex cursor-default items-center justify-center backdrop-blur-sm transition-colors ${
-          open ? 'visible bg-background/80 dark:bg-background/80' : 'invisible'
+  const body = document.querySelector('body');
+  // if (typeof window === 'object') {
+  return ReactDOM.createPortal(
+    // backdrop
+    <div
+      onClick={onClose}
+      className={`fixed inset-0 z-50 flex cursor-default items-center justify-center backdrop-blur-sm transition-colors ${
+        open ? 'visible bg-background/80 dark:bg-background/80' : 'invisible'
+      }`}
+      role='button'
+      tabIndex={-1}
+    >
+      {/* 關閉 X按鈕 */}
+      <button
+        className={`absolute right-1 top-2 bg-transparent p-1 text-black duration-500 hover:scale-125 dark:text-white lg:right-2 ${
+          open ? 'opacity-100' : 'opacity-0'
         }`}
-        role='button'
-        tabIndex={-1}
+        onClick={onClose}
       >
-        {/* 關閉 X按鈕 */}
-        <button
-          className={`absolute right-1 top-2 bg-transparent p-1 text-black duration-500 hover:scale-125 dark:text-white lg:right-2 ${
-            open ? 'opacity-100' : 'opacity-0'
-          }`}
-          onClick={onClose}
-        >
-          <X className='h-8 w-8' strokeWidth={3} />
-        </button>
-        {/* modal */}
-        <div
-          className={`h-auto w-full overflow-auto rounded-lg transition-all duration-500 max-sm:max-md:rounded-none ${
-            open ? 'scale-100 opacity-100' : 'scale-125 opacity-0'
-          }`}
-          onClick={(e) => e.stopPropagation()}
-          role='contentinfo'
-        >
-          {children}
-        </div>
-      </div>,
-      document.body,
-    );
-  }
+        <X className='h-8 w-8' strokeWidth={3} />
+      </button>
+      {/* modal */}
+      <div
+        className={`h-auto w-full overflow-auto rounded-lg transition-all duration-500 max-sm:max-md:rounded-none ${
+          open ? 'scale-100 opacity-100' : 'scale-125 opacity-0'
+        }`}
+        onClick={(e) => e.stopPropagation()}
+        role='contentinfo'
+      >
+        {children}
+      </div>
+    </div>,
+    body as Element,
+  );
+  // }
 };
 
 export default Modal;
