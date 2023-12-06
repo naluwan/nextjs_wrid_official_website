5db676321956215121104f759999e73270f43141
naluwan <street.baller06@gmail.com>
2023-12-06 17:37:39 +0800

    feat: add Contact page

---
+++ app/(routes)/contact/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PricingPage = () => {
+  return (
+    <div className='flex h-auto w-full items-center justify-center pt-20'>
+      <h1 className='text-4xl font-extrabold'>coming soon</h1>
+    </div>
+  );
+};
+
+export default PricingPage;
