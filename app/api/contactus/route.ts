d78510de6c6629ad5174283e84898ab1875cd442
naluwan <street.baller06@gmail.com>
2023-12-07 17:42:06 +0800

    feat: add api to send user contract info to official email

---
+++ app/api/contactus/route.ts
@@ -0,0 +1,43 @@
+import nodemailer from 'nodemailer';
+import { NextResponse } from 'next/server';
+
+export async function POST(req: Request) {
+  try {
+    const { name, email, phone, message } = await req.json();
+
+    if (!name || !email || !phone) {
+      return new NextResponse('姓名、電話和Email為必填欄位', { status: 400 });
+    }
+
+    // 郵件發送設定
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: process.env.EMAIL_ACCOUNT, // 替換成你的Gmail帳號
+        pass: process.env.EMAIL_PASSWORD, // 替換成你的Gmail密碼或應用程式密碼
+      },
+    });
+
+    // 郵件內容
+    const mailOptions = {
+      from: '吾境設計官網',
+      to: process.env.EMAIL_ACCOUNT, // 替換成你想要接收郵件的信箱
+      subject: `吾境設計 ${new Date(Date.now()).toLocaleDateString()} 有新顧客想要諮詢`,
+      text: `姓名: ${name}\nEmail: ${email}\n訊息: ${message}`,
+    };
+
+    // 發送郵件
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.error(error);
+        throw new Error('伺服器發生錯誤');
+      } else {
+        console.log('Email sent: ' + info.response);
+      }
+    });
+    return new NextResponse('已成功發送信件', { status: 200 });
+  } catch (err) {
+    console.log('[contactUs]', err);
+    return new NextResponse('伺服器發生錯誤', { status: 500 });
+  }
+}
