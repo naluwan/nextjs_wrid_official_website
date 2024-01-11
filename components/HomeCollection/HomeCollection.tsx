3b6eb7944fb5279f67ee477ad3551ddb2e6a55a0
naluwan <street.baller06@gmail.com>
2024-01-11 17:33:29 +0800

    fix: fix UI error

--- components/HomeCollection/HomeCollection.tsx
+++ components/HomeCollection/HomeCollection.tsx
@@ -29,49 +29,50 @@ const HomeCollection = () => {
   // }, []);
 
   return (
-    <section className='mt-8'>
-      <h2
-        className='mb-4 text-xl font-bold md:text-2xl'
-        data-aos='fade-up'
-        data-aos-delay='800'
-        data-aos-once
-      >
-        精選案例
-      </h2>
-      {/* 這裡可以使用輪播圖或其他方式展示案例 */}
-      <div
-        className='mb-4 grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3'
-        data-aos='fade-up'
-        data-aos-delay='600'
-        data-aos-once
-      >
-        {collections?.slice(0, 3).map((item) => (
-          <CollectionImage
-            img={item.images[0].src}
-            name={item.label}
-            key={item.id}
-            onClick={() => clickImage(item.images)}
-          />
-        ))}
-      </div>
+    <>
+      <section className='mt-8'>
+        <h2
+          className='mb-4 text-xl font-bold md:text-2xl'
+          data-aos='fade-up'
+          data-aos-delay='800'
+          data-aos-once
+        >
+          精選案例
+        </h2>
+        {/* 這裡可以使用輪播圖或其他方式展示案例 */}
+        <div
+          className='mb-4 grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3'
+          data-aos='fade-up'
+          data-aos-delay='600'
+          data-aos-once
+        >
+          {collections?.slice(0, 3).map((item) => (
+            <CollectionImage
+              img={item.images[0].src}
+              name={item.label}
+              key={item.id}
+              onClick={() => clickImage(item.images)}
+            />
+          ))}
+        </div>
 
-      <div
-        className='grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3'
-        data-aos='fade-up'
-        data-aos-delay='1000'
-        data-aos-once
-      >
-        {collections?.slice(4, 7).map((item) => (
-          <CollectionImage
-            img={item.images[0].src}
-            name={item.label}
-            key={item.id}
-            onClick={() => clickImage(item.images)}
-          />
-        ))}
-      </div>
+        <div
+          className='grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3'
+          data-aos='fade-up'
+          data-aos-delay='1000'
+          data-aos-once
+        >
+          {collections?.slice(4, 7).map((item) => (
+            <CollectionImage
+              img={item.images[0].src}
+              name={item.label}
+              key={item.id}
+              onClick={() => clickImage(item.images)}
+            />
+          ))}
+        </div>
 
-      {/* <DynamicModal
+        {/* <DynamicModal
         open={isModalOpen}
         onClose={() => {
           setIsModalOpen(false);
@@ -83,6 +84,7 @@ const HomeCollection = () => {
           key={selectedCollection?.id}
         />
       </DynamicModal> */}
+      </section>
       <Lightbox
         open={isModalOpen}
         close={() => setIsModalOpen(false)}
@@ -90,7 +92,7 @@ const HomeCollection = () => {
         // render={{ slide: NextJsImage }}
         plugins={[Zoom]}
       />
-    </section>
+    </>
   );
 };
 
