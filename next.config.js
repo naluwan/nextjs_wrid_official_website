007ee641750813f9bb2518c58037e45f6f23b950
naluwan <street.baller06@gmail.com>
2024-01-25 10:58:53 +0800

    fix: fix google map api key

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  env: {
+    NEXT_PUBLIC_GOOGLE_MAPS_API_KEY: 'AIzaSyBlLMJ7Scgd7DVgx8_OcaXb0kgO0u4sT8Q',
+  },
 };
 
 module.exports = nextConfig;
